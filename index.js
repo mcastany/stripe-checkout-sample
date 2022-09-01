c2220c64631c55590d853e8f1d314519cc9dc18a
Marcos Castany <marcos.castany@hotmail.com>
2022-09-01 23:14:01 +0200

    Updated views

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const stripe = require('stripe')(process.env.STRIPE_KEY);
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
