6aad62bcbb7548aae4a5e42b6774db930eee0a63
Marcos Castany <marcos.castany@hotmail.com>
2022-09-01 20:27:33 +0200

    Stripe checkout sample

---
+++ api/index.js
@@ -0,0 +1,82 @@
+require('dotenv').config()
+// This is your test secret API key.
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+const CURRENCY_SYMBOLS = {
+  'USD': '$', // US Dollar
+  'EUR': '€', // Euro
+  'CRC': '₡', // Costa Rican Colón
+  'GBP': '£', // British Pound Sterling
+  'ILS': '₪', // Israeli New Sheqel
+  'INR': '₹', // Indian Rupee
+  'JPY': '¥', // Japanese Yen
+  'KRW': '₩', // South Korean Won
+  'NGN': '₦', // Nigerian Naira
+  'PHP': '₱', // Philippine Peso
+  'PLN': 'zł', // Polish Zloty
+  'PYG': '₲', // Paraguayan Guarani
+  'THB': '฿', // Thai Baht
+  'UAH': '₴', // Ukrainian Hryvnia
+  'VND': '₫', // Vietnamese Dong
+};
+
+app.get('/', async (req, res) => {
+  const products = await stripe.products.list({
+    limit: 3,
+  });
+
+  const prices = await stripe.prices.list({
+    limit: 3,
+  });
+
+  prices.data.forEach((p) => {
+    p.product = products.data.filter(prod => prod.id === p.product)[0];
+  });
+
+
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.render('checkout', {
+    prices: prices.data || [],
+    CURRENCY_SYMBOLS: CURRENCY_SYMBOLS
+  });
+})
+
+app.get('/success', async (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.render('success');
+})
+
+app.get('/cancel', async (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.render('cancel');
+})
+
+app.post('/create-checkout-session/:price_id', async (req, res) => {
+  if (!req.params.price_id){
+    res.redirect(303, '/');
+    return;
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [{
+      price: req.params.price_id,
+      quantity: 1,
+    }, ],
+    mode: 'subscription',
+    success_url: `${YOUR_DOMAIN}/success`,
+    cancel_url: `${YOUR_DOMAIN}/cancel`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
